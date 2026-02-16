6a047827ca6bd2ac2d5745657226779ea5c55aeb
814725243015 <814725243015@trp.srmtrichy.edu.in>
2026-02-16 14:48:22 +0530

    Update string.py

--- string.py
+++ string.py
@@ -1,4 +1,11 @@
-s1= python
-s2= programming
+s1= "python"
+s2= "programming"
 print("string s1:", s1)
 print("string s2:", s2)
+print("concatenation:", s1+s2)
+print("concatenation:", s1+" "+s2)
+print("lenth of string 1 is :" len(s1))
+print("lenth of string 2 is :" len(s2))
+print("first character of string 1:", s(0))
+print("last character of string 1:", s(-1))
+print("substring of string:", s[0:3])
